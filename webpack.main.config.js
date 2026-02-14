16dd0daa0cc1a8200441cb3a4c7ef9e814c57517
Anshu Mandal <anshumandal299@gmail.com>
2026-02-14 22:34:58 +0530

    Refactor code structure for improved readability and maintainability

--- webpack.main.config.js
+++ webpack.main.config.js
@@ -26,12 +26,12 @@ module.exports = {
   resolve: {
     extensions: [".ts", ".js", ".json"],
   },
-  externals: {
-    "electron-store": "commonjs electron-store",
-  },
+
   plugins: [
     new webpack.DefinePlugin({
-      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV || "production"),
+      "process.env.NODE_ENV": JSON.stringify(
+        process.env.NODE_ENV || "production"
+      ),
     }),
   ],
   node: {
